2313ec8f94b556b1aeebcb0c305027a5352dd4b7
Tyler Hardison <tylerh@evercharge.com>
2023-01-11 11:21:26 -0800

    remove unneeded

--- src/lib/postgres/index.ts
+++ src/lib/postgres/index.ts
@@ -54,6 +54,7 @@ export default class PostgresStats {
       return;
     }
     timestamp = Math.round(timestamp);
+    delete song.avatar;
     await this.client("songs").insert({ ...song }).onConflict(["url"]).merge();
     return this.client("playing").insert({ uid, timestamp: this.client.raw('to_timestamp(?)', [timestamp]), url: song.url });
     
