ea46c44d09464d0a91761c95cd8c9dca5b7433ed
Tyler Hardison <tylerh@evercharge.com>
2023-01-11 14:32:33 -0800

    gates

--- src/statbot/modules/stats.ts
+++ src/statbot/modules/stats.ts
@@ -1,41 +1,55 @@
 export default async function gatherStats(chat: any, message: any) {
+  const playingHours = await chat.postgres.playingHours(message.uid);
+  const chatStats = await chat.postgres.getChatStats(message.uid);
+  const topSong = chat.postgres.getTopSong(message.uid);
+  const mostLikedSong = chat.postgres.getMostLikedSong(message.uid);
 
-    const playingHours = await chat.postgres.playingHours(message.uid);
-    const chatStats = await chat.postgres.getChatStats(message.uid);
-    const topSong = chat.postgres.getTopSong(message.uid);
-    const mostLikedSong = chat.postgres.getMostLikedSong(message.uid);
+  var chatMsg = `==markdown==\n## ${message.username}'s stats\n\n`;
+  chatMsg += `### Music\n\n`;
+  if (playingHours.rows.length > 0)
+    chatMsg += `You have played ${Math.floor(
+      playingHours.rows[0].total / 60 / 60 / 1000
+    )}:${
+      Math.floor(playingHours.rows[0].total / 60 / 1000) % 60 < 10 ? "0" : ""
+    }${
+      Math.floor(playingHours.rows[0].total / 60 / 1000) % 60
+    } hours of music.\n\n`;
+  if (topSong.rows.length > 0)
+    chatMsg += `Your most played song is ${topSong.rows[0].title} with ${topSong.rows[0].count} plays.\n\n`;
+  if (mostLikedSong.rows.length > 0)
+    chatMsg += `Your most liked song is ${mostLikedSong.rows[0].title} with ${mostLikedSong.rows[0].count} likes.\n\n`;
 
-
+  if (chatStats.rows.length > 0) {
     var mostActiveHour: number;
-            
+
     mostActiveHour = parseInt(chatStats.rows[0].most_active_hour) + 8;
 
     if (mostActiveHour > 24) {
       mostActiveHour = mostActiveHour - 24;
     }
 
-    var chatMsg = `==markdown==\n## ${message.username}'s stats\n\n`;
-    chatMsg += `### Music\n\n`;
-    chatMsg += `You have played ${Math.floor(playingHours.rows[0].total / 60 / 60 / 1000)}:${Math.floor(playingHours.rows[0].total / 60 / 1000) % 60 < 10 ? "0" : ""}${Math.floor(playingHours.rows[0].total / 60 / 1000) % 60} hours of music.\n\n`;
-    chatMsg += `Your most played song is ${topSong.rows[0].title} with ${topSong.rows[0].count} plays.\n\n`;
-    chatMsg += `Your most liked song is ${mostLikedSong.rows[0].title} with ${mostLikedSong.rows[0].count} likes.\n\n`;
-    
     chatMsg += `### Chat\n\n`;
     chatMsg += `You have chatted for 
-    ${chatStats.rows[0].total_time.hours ? chatStats.rows[0].total_time.hours : "00"}:
+    ${
+      chatStats.rows[0].total_time.hours
+        ? chatStats.rows[0].total_time.hours
+        : "00"
+    }:
     ${chatStats.rows[0].total_time.minutes < 10 ? "0" : ""}
     ${chatStats.rows[0].total_time.minutes}:
     ${chatStats.rows[0].total_time.seconds < 10 ? "0" : ""}
-    ${chatStats.rows[0].total_time.seconds} minutes with your most active hour being 
+    ${
+      chatStats.rows[0].total_time.seconds
+    } minutes with your most active hour being 
     ${mostActiveHour}:00 UTC.\n\n`;
-    
-    chat.pushChatMsg(
-        {
-            username: chat.chatConfig.username,
-            msg: chatMsg,
-        },
-        chat.chatConfig.user
-    );
+  }
+  chat.pushChatMsg(
+    {
+      username: chat.chatConfig.username,
+      msg: chatMsg,
+    },
+    chat.chatConfig.user
+  );
 }
 //     if (!hours && !hours[0] && !hours[0].total) {
 //             chat.pushChatMsg(
@@ -57,7 +71,7 @@ export default async function gatherStats(chat: any, message: any) {
 //                   Math.floor(hours.rows[0].total / 60 / 1000) % 60 < 10 ? "0" : ""
 //                 }${
 //                   Math.floor(hours.rows[0].total / 60 / 1000) % 60
-//                 } hours of music.\nYou have chatted for 
+//                 } hours of music.\nYou have chatted for
 //                 ${stats.rows[0].total_time.hours ? stats.rows[0].total_time.hours : "00"}:${
 //                   stats.rows[0].total_time.minutes < 10 ? "0" : ""
 //                 }${stats.rows[0].total_time.minutes}:${
