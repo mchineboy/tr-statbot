fbe4e083110b4b2444bcc61543ad8c6269394d44
Tyler Hardison <tylerh@evercharge.com>
2023-01-11 14:59:59 -0800

    clean up stats

--- src/statbot/modules/stats.ts
+++ src/statbot/modules/stats.ts
@@ -7,42 +7,43 @@ export default async function gatherStats(chat: any, message: any) {
   var chatMsg = `==markdown==\n## ${message.username}'s stats\n\n`;
   chatMsg += `### Music\n\n`;
   if (playingHours && playingHours.rows?.length > 0)
-    chatMsg += `You have played ${Math.floor(
+    chatMsg += `* You have played ${Math.floor(
       playingHours.rows[0].total / 60 / 60 / 1000
-    )}:${
+    )}h ${
       Math.floor(playingHours.rows[0].total / 60 / 1000) % 60 < 10 ? "0" : ""
     }${
       Math.floor(playingHours.rows[0].total / 60 / 1000) % 60
-    } hours of music.\n\n`;
+    }m of music.\n`;
   if (topSong && topSong.rows?.length > 0)
-    chatMsg += `Your most played song is ${topSong.rows[0].title} with ${topSong.rows[0].count} plays.\n\n`;
+    chatMsg += `* Your most played song is ${topSong.rows[0].title} with ${topSong.rows[0].count} plays.\n`;
   if (mostLikedSong && mostLikedSong.rows?.length > 0)
-    chatMsg += `Your most liked song is ${mostLikedSong.rows[0].title} with ${mostLikedSong.rows[0].count} likes.\n\n`;
+    chatMsg += `* Your most liked song is ${mostLikedSong.rows[0].title} with ${mostLikedSong.rows[0].count} likes.\n\n`;
 
   if (chatStats && chatStats.rows?.length > 0) {
     console.log(JSON.stringify(chatStats.rows, undefined, 2))
     var mostActiveHour: number;
 
-    mostActiveHour = parseInt(chatStats.rows[0].most_active_hour) + 8;
+    mostActiveHour = parseInt(chatStats.rows[0].most_active_hour)
+    mostActiveHour += 8;
 
     if (mostActiveHour > 24) {
       mostActiveHour = mostActiveHour - 24;
     }
 
     chatMsg += `### Chat\n\n`;
-    chatMsg += `You have chatted for ${
+    chatMsg += `* You have chatted for ${
       chatStats.rows[0].total_time.hours
         ? chatStats.rows[0].total_time.hours
         : "00"
-    }:${
+    }h ${
         chatStats.rows[0].total_time.minutes < 10 ? "0" : ""
     }${
         chatStats.rows[0].total_time.minutes
-    }:${
+    }m ${
         chatStats.rows[0].total_time.seconds < 10 ? "0" : ""
     }${
       chatStats.rows[0].total_time.seconds
-    } minutes with your most active hour being ${mostActiveHour}:00 UTC.\n\n`;
+    }s\n* your most active hour is ${mostActiveHour}:00 UTC.\n\n`;
   }
   chat.pushChatMsg(
     {
