161dcec099c1d67cc7297deb9a4177d5c50fd5d3
Tyler Hardison <tylerh@evercharge.com>
2023-01-11 09:33:34 -0800

    remove function

--- src/lib/postgres/index.ts
+++ src/lib/postgres/index.ts
@@ -30,36 +30,6 @@ export default class PostgresStats {
     for (const schema in schemas) {
       await schemas[schema](this.client);
     }
-    await this.client.raw(
-      `CREATE OR REPLACE FUNCTION calculate_time_consumed_and_most_active_hours_single_uid(tbl_name text, user_id int)
-              RETURNS TABLE(uid int, total_time interval, most_active_hours int) AS $$
-              BEGIN
-                  RETURN QUERY
-                  WITH time_diff AS (
-                      SELECT uid, timestamp, lead(timestamp) OVER (PARTITION BY uid ORDER BY timestamp) - timestamp as diff
-                      FROM tbl_name
-                      WHERE uid = user_id
-                  ),
-                  hourly_activity AS (
-                      SELECT uid, date_trunc('hour', timestamp) as hour, sum(diff) as time_diff
-                      FROM time_diff
-                      WHERE diff < interval '15 minutes' OR diff IS NULL
-                      GROUP BY uid, hour
-                  ),
-                  max_hourly_activity AS (
-                      SELECT uid, max(time_diff) as max_time_diff
-                      FROM hourly_activity
-                      GROUP BY uid
-                  )
-                  SELECT ha.uid, SUM(ha.time_diff) as total_time, EXTRACT(HOUR FROM ha.hour) as most_active_hours
-                  FROM hourly_activity ha
-                  JOIN max_hourly_activity ma ON ha.uid = ma.uid AND ha.time_diff = ma.max_time_diff
-                  GROUP BY ha.uid, ha.hour
-                  ORDER BY most_active_hours DESC;
-              END;
-              $$ LANGUAGE plpgsql;
-              `
-    );
 
     this.isInitialized = true;
   }
