49bdaf8d4e5a99315d87a108543f4e0180bc1bf1
Tyler Hardison <tylerh@evercharge.com>
2023-02-09 16:36:56 -0800

    fix storage of envvars for production

--- src/env.ts
+++ src/env.ts
@@ -37,6 +37,14 @@ if (!_env.success) {
   fail(...formatErrors(_env.error.format()));
 }
 
+// Production stores the service account as a BASE64 encoded string
+
+try {
+  _env.data.FBASE_SERVICE = Buffer.from(_env.data.FBASE_SERVICE, "base64").toString();
+} catch (e) {
+  // ignore
+}
+
 try {
   JSON.parse(_env.data.FBASE_SERVICE);
 } catch (e) {
